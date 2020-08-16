6b06f0933e938b6180cc50ccf09dbc6aa77550d0
tata-prog <mandoladopacificojr@gmail.com>
2020-08-16 21:47:30 +0800

    Updates

---
+++ precache-manifest.10dc0ea8a5cb03819985b8dd266ccd81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fa9cdb65818c8b63516a3e84433afa",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "5cdcb83c69be4c62eba2",
+    "url": "/blackjack/static/css/main.34da8c3c.chunk.css"
+  },
+  {
+    "revision": "954e6ec3ecaaf2d5cba0",
+    "url": "/blackjack/static/js/2.9f99aa67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blackjack/static/js/2.9f99aa67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdcb83c69be4c62eba2",
+    "url": "/blackjack/static/js/main.da45ecf5.chunk.js"
+  },
+  {
+    "revision": "68734dc473247443cdbc",
+    "url": "/blackjack/static/js/runtime-main.852dc962.js"
+  }
+]);
