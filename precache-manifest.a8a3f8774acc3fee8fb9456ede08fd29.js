bac65d6e38192b01e3dcff420d907defbd0f6eb3
tata-prog <mandoladopacificojr@gmail.com>
2020-08-16 21:52:51 +0800

    Updates

---
+++ precache-manifest.a8a3f8774acc3fee8fb9456ede08fd29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d022c9f98528129d8bb4b01c9283d741",
+    "url": "/github.io/blackjack/index.html"
+  },
+  {
+    "revision": "5cdcb83c69be4c62eba2",
+    "url": "/github.io/blackjack/static/css/main.34da8c3c.chunk.css"
+  },
+  {
+    "revision": "954e6ec3ecaaf2d5cba0",
+    "url": "/github.io/blackjack/static/js/2.9f99aa67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/blackjack/static/js/2.9f99aa67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdcb83c69be4c62eba2",
+    "url": "/github.io/blackjack/static/js/main.da45ecf5.chunk.js"
+  },
+  {
+    "revision": "d62130c50f51697c244c",
+    "url": "/github.io/blackjack/static/js/runtime-main.119fb571.js"
+  }
+]);
